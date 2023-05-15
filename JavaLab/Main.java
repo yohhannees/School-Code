4573f80dcc74624d1e2aec23f44dca59e23409ee
Yohannees <yohannesgetachewerieso@gmail.com>
2023-05-15 16:33:29 +0300

    updated

---
+++ JavaLab/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        Multi2 m2 = new Multi2();
+        Thread Th2 =new Thread();
+        Th2.start();
+    }
+}
---
+++ JavaLab/Multi2.java
@@ -0,0 +1,9 @@
+public class Multi2 implements Runnable {
+
+    @Override
+    public void run() {
+  
+        System.out.println("Thread is running");
+    }
+
+}
